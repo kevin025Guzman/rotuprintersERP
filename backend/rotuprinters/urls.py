3d8f4bf3d74e6795af96f0af2c26c1e105af462e
Kevin Guzmán <guzmankevin025@gmail.com>
2025-11-19 20:39:00 -0600

    Agregado build del frontend

--- backend/rotuprinters/urls.py
+++ backend/rotuprinters/urls.py
@@ -2,7 +2,8 @@
 URL configuration for rotuprinters project.
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
+from django.views.generic import TemplateView
 from django.conf import settings
 from django.conf.urls.static import static
 from rest_framework import permissions
@@ -39,6 +40,7 @@ urlpatterns = [
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
     path('swagger.json', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    re_path(r'^.*$', TemplateView.as_view(template_name='index.html'), name='spa-entry'),
 ]
 
 if settings.DEBUG:
