f40311152fb3c335686944c8c893fa0ec054831b
Kevin Guzmán <guzmankevin025@gmail.com>
2025-11-19 20:57:19 -0600

    vite config frontend

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+const isProduction = process.env.NODE_ENV === 'production'
+
 export default defineConfig({
+  base: isProduction ? '/static/' : '/',
   plugins: [react()],
   server: {
     host: '0.0.0.0',
